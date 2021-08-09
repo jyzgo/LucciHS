3cc8910662d337d8120163e94d5272062bbc26fd
ethanjiang <ethanjyzgo@gmail.com>
2021-08-09 17:18:26 +0800

    update

--- Assets/xShifter/Scripts/RewardCheatBtn.cs
+++ Assets/xShifter/Scripts/RewardCheatBtn.cs
@@ -1,3 +1,4 @@
+using EasyUI.Dialogs;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,20 @@ public class RewardCheatBtn :BtnExtension
 {
     protected override void OnPressed()
     {
-        AdsMgr.current.ShowRewardAds(DoNothing);
-    }
-    void DoNothing()
+        ConfirmDialogUI.Instance
+.SetTitle("Ads")
+.SetMessage("Do you want to play ads to unlock this?")
+.SetButtonsColor(DialogButtonColor.Yellow)
+.SetFadeDuration(.6f)
+.OnPositiveButtonClicked(() =>   AdsMgr.current.ShowRewardAds(CallBack))
+.Show();
+
+          }
+    public string msg;
+    protected void CallBack()
     {
+
+        TimeLineMgr.current.PlayingTimeLine(msg);
     }
 
 }
